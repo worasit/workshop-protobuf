71d30dd63a96000e39c73326a0dbd8c38d4b9f3c
worasit <worasit.daimongkol@thoughtworks.com>
2021-11-22 14:10:19 +0700

    ignore slf4j

--- src/main/java/com/daimongkol/protobuf/ComplexMain.java
+++ src/main/java/com/daimongkol/protobuf/ComplexMain.java
@@ -1,9 +1,12 @@
 package com.daimongkol.protobuf;
 
 import com.daimongkol.protobuf.models.Student;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class ComplexMain {
     public static void main(String[] args) {
+        log.info("Start Complex Main");
         Student student = Student.builder()
                 .id(1)
                 .firstName("worasit")
